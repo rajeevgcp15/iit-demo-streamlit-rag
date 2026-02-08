2767831ed0ef0c29b758b75e3db4317747397708
rajeevgcp15 <106531460+rajeevgcp15@users.noreply.github.com>
2026-02-08 16:17:00 +0530

    Update rag-iit.py

--- rag-iit.py
+++ rag-iit.py
@@ -6,7 +6,7 @@ import os
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.vectorstores import FAISS
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains import RetrievalQA
 
 # ------------------ PAGE CONFIG ------------------
